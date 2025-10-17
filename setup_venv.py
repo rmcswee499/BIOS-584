bc2de4f8eba1b26e356eb0fa06e788f6ca1c3ccf
rmcswee499 <rebecca.mcsweeney@emory.edu>
2025-10-17 09:08:46 -0400

    HW7 beginning

---
+++ setup_venv.py
@@ -0,0 +1,7 @@
+required_packages = ['numpy','scipy','pandas','matplotlib','openpyxl','jupyter','scikit-learn']
+import pip
+
+for n_iter in range(len(required_packages)):
+    pip install required_packages
+
+print("Packages installed successfully")
